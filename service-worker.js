479a9f722bac337165f3e20dd6858534c98445a2
Ruben D Moser <rubend moser@gmail.com>
2026-06-24 01:38:18 -0300

    Mejorar diseño y texto institucional

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "km-detail-shop-v26";
+const CACHE_NAME = "km-detail-shop-v27";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=28",
-  "./app.js?v=28",
+  "./styles.css?v=29",
+  "./app.js?v=29",
   "./assets/km-hero-detailing.png",
   "./assets/catalogo-2026/1_DETAIL-LINE.png",
   "./reset.html",
